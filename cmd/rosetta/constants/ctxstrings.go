8a19457a673a63a02ccf56273345267532b88e88
Bryan Riley <brileydeveloper@gmail.com>
2020-01-20 02:13:11 -0500

    Addressing issue #3. Created module for default context strings

--- cmd/rosetta/constants/ctxstrings.go
+++ cmd/rosetta/constants/ctxstrings.go
@@ -1,3 +1,12 @@
+//Default strings for initial release will be backend, frontent, devops
+
 package constants
 
-//Default strings for initial release will be backend, frontent, devops
+// DevopsContextName is the name for the devops context
+const DevopsContextName = "devops"
+
+// BackendContextName is the name for the backend context
+const BackendContextName = "backend"
+
+// FrontendContextName is the name for frontend context
+const FrontendContextName = "frontend"
